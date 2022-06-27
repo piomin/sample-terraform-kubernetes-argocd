c86ad940cf43eeb6d8f92191d4959d7ae3f66f17
piomin <piotr.minkowski@gmail.com>
2022-06-28 00:21:56 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "tehcyx/kind"
+      version = "0.0.12"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "default" {
+  name = "cluster-1"
+  wait_for_ready = true
+  kind_config {
+    kind = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+
+    node {
+      role = "worker"
+      image = "kindest/node:v1.24.2"
+    }
+
+    node {
+      role = "worker"
+      image = "kindest/node:v1.24.2"
+    }
+
+    node {
+      role = "worker"
+      image = "kindest/node:v1.24.2"
+    }
+  }
+}
+
+provider "kubectl" {
+  host = "${kind_cluster.default.endpoint}"
+  cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
+  client_certificate = "${kind_cluster.default.client_certificate}"
+  client_key = "${kind_cluster.default.client_key}"
+}
+
+data "kubectl_file_documents" "crds" {
+  content = file("olm/crds.yaml")
+}
+
+resource "kubectl_manifest" "crds_apply" {
+  for_each  = data.kubectl_file_documents.crds.manifests
+  yaml_body = each.value
+  wait = true
+  server_side_apply = true
+}
+
+data "kubectl_file_documents" "olm" {
+  content = file("olm/olm.yaml")
+}
+
+resource "kubectl_manifest" "olm_apply" {
+  depends_on = [data.kubectl_file_documents.crds]
+  for_each  = data.kubectl_file_documents.olm.manifests
+  yaml_body = each.value
+}
+
+provider "helm" {
+  kubernetes {
+    host = "${kind_cluster.default.endpoint}"
+    cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
+    client_certificate = "${kind_cluster.default.client_certificate}"
+    client_key = "${kind_cluster.default.client_key}"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name  = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "4.9.7"
+  create_namespace = true
+
+  values = [
+    file("argocd/application.yaml")
+  ]
+}
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Manage Kubernetes Cluster with Terraform and Argo CD [![Twitter](https://img.shields.io/twitter/follow/piotr_minkowski.svg?style=social&logo=twitter&label=Follow%20Me)](https://twitter.com/piotr_minkowski)
+
+In this project I'm demonstrating you how to use [Terraform](https://www.terraform.io/) together with [Argo CD](https://argo-cd.readthedocs.io/en/stable/) to create and manage the Kubernetes cluster on [Kind](https://kind.sigs.k8s.io/).
+
+## Prerequisites
+1. Terraform CLI installed
+2. Docker
+
+## Getting Started
+
+You may the detailed explanation of that example repository in the following article: will be published soon...
+
+First, clone that repo:
+```shell
+$ git clone https://github.com/piomin/sample-terraform-kubernetes-argocd.git
+$ cd sample-terraform-kubernetes-argocd
+```
+
+Then initialize Terraform config: \
+```shell
+terraform init
+```
+
+Review the actions plan: \
+```shell
+terraform plan
+```
+
+Run the Terraform actions: \
+```shell
+terraform apply
+```
+
+## Results
+
+After running the previous command you receive:
+* 3-nodes Kind cluster running locally
+* OLM (Operator Lifecycle Manager) installed on Kind
+* Argo CD installed on Kind
+* Kafka Strimzi operator ready to use
+* 3-node Kafka cluster created on Kind
